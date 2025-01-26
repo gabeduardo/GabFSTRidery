26c89cc450bf89e6f1ac8f3f94fde9f25383cb92
gmdarient <gmedina@darient.com>
2025-01-26 19:10:10 -0400

    agregando funcionalidad para cambiar status desde el home

--- frontVue/src/components/Home.vue
+++ frontVue/src/components/Home.vue
@@ -9,7 +9,13 @@
           </div>
         </v-card-title>
         <v-card-text>
-          Estado: {{ vehicle.status }}
+          Estado: 
+          <v-select
+            v-model="vehicle.status"
+            :items="statuses"
+            label="Cambiar Estado"
+          ></v-select>
+          <v-btn @click="updateStatus(vehicle._id, vehicle.status)">Actualizar</v-btn>
         </v-card-text>
       </v-card>
     </v-flex>
@@ -19,8 +25,10 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const vehicles = ref([]);
+const statuses = ['disponible', 'en mantenimiento', 'en servicio'];
 
 const fetchVehicles = async () => {
   try {
@@ -28,6 +36,35 @@ const fetchVehicles = async () => {
     vehicles.value = response.data;
   } catch (error) {
     console.error('Error fetching vehicles:', error);
+  }
+};
+
+// Actualiza el estado del vehículo utilizando la nueva ruta del endpoint
+const updateStatus = async (vehicleId, newStatus) => {
+  try {
+    await axios.put(`http://localhost:3000/vehicles/${vehicleId}/status`, {
+      status: newStatus,
+      updatedBy: '60d5ec49bcf86cd799439020', //TODO  Este valor debería ser dinámico 
+    }, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    // Mensaje de éxito usando SweetAlert2
+    Swal.fire({
+      icon: 'success',
+      title: 'Éxito',
+      text: 'Estado del vehículo actualizado exitosamente!',
+    });
+  } catch (error) {
+    console.error('Error actualizando el estado del vehículo:', error);
+
+    // Capturar y mostrar el mensaje de error del backend
+    const errorMessage = error.response?.data?.error || 'Hubo un problema actualizando el estado del vehículo. Por favor, inténtalo de nuevo.';
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: errorMessage,
+    });
   }
 };
 
